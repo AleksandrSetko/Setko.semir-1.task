d6a2f0308de8732a0e0ad3fa62e957b5db005dd1
Setko AI <setko@msn.com>
2024-05-28 12:29:05 +0300

    Lesson 2 lecture

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = number / 10 % 10;
+    int lastDigit = number % 10;
+    int result = 1;
+
+    for (int i = 0; i < lastDigit; i++)
+    {
+        result = result * secondDigit;
+    }
+
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
+
