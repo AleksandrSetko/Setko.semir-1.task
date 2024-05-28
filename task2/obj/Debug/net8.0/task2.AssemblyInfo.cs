2ad0e9c5a484c3a5fcbf97c53853cf6dc37ba4fa
Setko AI <setko@msn.com>
2024-05-28 15:02:36 +0300

    deleted

--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a2f0308de8732a0e0ad3fa62e957b5db005dd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dbaf7fbfab6fbeedc5f442e41e802d9bbd5243e")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
